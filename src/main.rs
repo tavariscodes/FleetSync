fc5820659d3e16bb5e2bf0ebaf9dd2f779690d4e
tavariscodes <tavariscodes@gmail.com>
2025-04-30 18:37:22 -0500

    add road code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use eframe::egui::{self, Color32};
+use eframe::egui::{self, Color32, Pos2, Rect, Stroke};
+use env_logger::fmt::style::Color;
 use uuid::Uuid;
 mod canvas;
 
@@ -62,6 +63,31 @@ impl App {
             let rect = ui.max_rect();
             let painter = ui.painter();
             painter.rect_filled(rect, 0.0, Color32::from_gray(30));
+
+            let road_rect = Rect::from_min_size(
+                Pos2::new(rect.left() + 100.0, rect.top() + 200.0),
+                egui::vec2(600.0, 60.0),
+            );
+
+            painter.rect_filled(road_rect, 0.0, Color32::DARK_GRAY);
+
+            // draw centered white line
+
+            let lane_y = road_rect.center().y;
+            let lane_x_start = road_rect.left() + 20.0;
+            let lane_x_end = road_rect.right() - 20.0;
+
+            let mut x = lane_x_start;
+            while x < lane_x_end {
+                painter.line_segment(
+                    [
+                        Pos2::new(x, lane_y - 1.0),
+                        Pos2::new(x + 10.0, lane_y - 1.0),
+                    ],
+                    Stroke::new(2.0, Color32::WHITE),
+                );
+                x+=20.0;
+            }
         });
     }
 
